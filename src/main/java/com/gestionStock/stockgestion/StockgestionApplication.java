8fa39d7cf08949564e309d4bc62e3c78f0a0141f
Fuji Vanilli <ijufillivan@gmail.com>
2023-01-28 11:51:42 +0300

    first commit

---
+++ src/main/java/com/gestionStock/stockgestion/StockgestionApplication.java
@@ -0,0 +1,13 @@
+package com.gestionStock.stockgestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockgestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockgestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionStock/stockgestion/StockgestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionStock.stockgestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockgestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
